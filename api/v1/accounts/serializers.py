b01ab37b00e623e21262d7b7b2e599663c4d4356
Mhdnisam <mhdnisamm@gmail.com>
2026-01-27 15:33:04 +0530

    Fix signup permissions

--- api/v1/accounts/serializers.py
+++ api/v1/accounts/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -12,7 +11,7 @@ class SignupSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],          # ✅ correct
-            password=validated_data["password"],    # ✅ only once
+            email=validated_data["email"],
+            password=validated_data["password"],
         )
         return user
