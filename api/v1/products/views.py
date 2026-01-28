f995ddde3e6fc0d77ce4506665b29d4b58e00672
Mhdnisam <mhdnisamm@gmail.com>
2026-01-28 11:29:32 +0530

    AllowAny for products

--- api/v1/products/views.py
+++ api/v1/products/views.py
@@ -5,16 +5,18 @@ from .serializers import ProductListSerializer, ProductDetailSerializer
 
 from rest_framework.generics import ListCreateAPIView
 
+from rest_framework.permissions import AllowAny
+
 class ProductListAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]   # ✅ TEMP FIX
+
 
 
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-
 class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
