b96819d2f9f35b9bb421749630e0961fa1010fcc
Mhdnisam <mhdnisamm@gmail.com>
2026-01-29 16:32:00 +0530

    added image in models

--- api/v1/products/serializers.py
+++ api/v1/products/serializers.py
@@ -2,10 +2,25 @@ from rest_framework import serializers
 from products.models import Product
 
 
+from rest_framework import serializers
+from .models import Product
+
 class ProductListSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price']
+        fields = ['id', 'name', 'price', 'image_url']
+
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url)
+
+        # ✅ Fixed default image
+        return request.build_absolute_uri("/static/products/default.jpg")
+
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
